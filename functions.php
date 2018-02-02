1ede94673a98a8febf57bc42009582468654449e
marceliotstein <marc.eliot.stein@gmail.com>
2018-02-02 16:34:19 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Apostrophe functions and definitions
+ *
+ * @package Apostrophe
+ */
+
+function apostrophe_enqueue_styles() {
+  $parent_style = 'apostrophe-style';
+  wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array($parent_style),
+    wp_get_theme()->get('Version')
+  );
+}
+add_action('wp_enqueue_scripts', 'apostrophe_enqueue_styles');
+
+/*
+ * remove Related Posts, which is added by Jetpack
+ */
+
+function jetpackme_remove_rp() {
+    if ( class_exists( 'Jetpack_RelatedPosts' ) ) {
+        $jprp = Jetpack_RelatedPosts::init();
+        $callback = array( $jprp, 'filter_add_target_to_dom' );
+        remove_filter( 'the_content', $callback, 40 );
+    }
+}
+add_filter( 'wp', 'jetpackme_remove_rp', 20 );
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+=== Apostrophe Child Theme for AllenGinsberg.org ===
+
+This is a child theme of Apostrophe designed by Marc Eliot Stein for the Allen Ginsberg Project
+
+
